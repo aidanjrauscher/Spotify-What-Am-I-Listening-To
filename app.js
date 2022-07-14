8004837b89063b97bddd09414bd43d1d03fe1da5
Aidan Rauscher <73447549+aidanjrauscher@users.noreply.github.com>
2022-07-14 14:05:50 -0400

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const https = require("https");
+const express = require("express");
+const axios = require("axios");
+const cookieParser = require('cookie-parser');
+
+const funcs = require("./funcs");
+
+
+const hostname = '127.0.0.1' 
+const port = '3000'
+const interval = 3 * 60 * 1000 //3 minutes
+
+const app = express()
+
+app.get('/',async (req,res)=>{
+
+    let track_info = await funcs.getTrack();
+    res.send(`<h1>${track_info.track_status}</h1><br>` +
+            `<h2>Track Name: ${track_info.track_name}</h2><br>`+
+            `<h3>Album Name: ${track_info.album_name}</h2><br>`+
+            `<p>Artist Name(s):<br> ${track_info.artists.join('<br>')}</h2><br><br>`+
+            `<a href="${track_info.track_url}" target="_blank">` + 
+                `<img  style="height: 320px; width:320px; "src="${track_info.image}">` + 
+            `</a>`);
+
+})
+
+app.listen(port, ()=>{
+    console.log(`Server listening on http://${hostname}:${port}`)
+})
+
---
+++ funcs.js
@@ -0,0 +1,103 @@
+const axios = require("axios");
+const { syncBuiltinESMExports } = require("module");
+require('dotenv').config();
+const qs = require("qs");
+
+const client_id = process.env.SPOTIFY_API_ID; // Your client id
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
+const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN
+const auth_token = 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
+
+let auth = async ()=>{
+    try{
+        const url =  'https://accounts.spotify.com/api/token'
+        const auth_response = await axios.post(url,
+            qs.stringify({'grant_type': 'refresh_token','refresh_token': refresh_token}),
+            {
+                headers:{
+                'Authorization': auth_token,
+                'Content-Type': 'application/x-www-form-urlencoded' 
+            }}
+        )
+        return auth_response.data.access_token
+    }
+    catch(error){
+        console.log('Spotify API authorization: ' + error)
+    }
+}
+
+let currentTrack = async (token)=>{
+    const current_track_url = 'https://api.spotify.com/v1/me/player/currently-playing'
+    try{
+        const track_response = await axios.get(current_track_url, {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+        return track_response.data; 
+    }
+    catch(error){
+        console.log('Error getting playback state: ' + error)
+        return '' 
+    }
+
+}
+
+let recentTrack = async (token)=>{
+    const recent_track_url = 'https://api.spotify.com/v1/me/player/recently-played'
+    try{
+        const tracks_response = await axios.get(recent_track_url, {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+        return tracks_response.data.items[0].track; 
+    }
+    catch(error){
+        console.log('Error getting playback state: ' + error)
+        return '' 
+    }
+
+}
+
+let getTrack = async ()=>{
+    const token = await auth()
+     //token = 'BQAMg0GIB9Rs9CEsA98XEsH4libTMc6XOUnLDuvKEcm_T25ZXYkYdze0gL5Ry_t9hcaXjd5KKCmzwQpwkDjnvbQG4B8FsG4OMh5cHDidjp1XF-0MtfeT4IH1CieE35D_urp2vFkCSN0dpaR9hOfCGcoOcXtH8Z1aMC0sm-oVSsWH0-1vCC7s5TTgLIic7cwW6u8x826NEA'; 
+    const current_track = await currentTrack(token)
+    let track; 
+    let track_status; 
+    if(current_track=='' || current_track.currently_playing_type != 'track'){
+        const recent_track = await recentTrack(token)
+        track = recent_track;
+        track_status = 'Recently Listened To'
+    }
+    else{
+        track = current_track.item; 
+        track_status = 'Currently Listening To'
+    }
+    let track_url = track.external_urls.spotify
+    let track_name = track.name
+    let album_name = track.album.name
+    let artist_object = track.artists
+    let artist_array = []
+    for(let i=0; i<artist_object.length; i++){
+        artist_array.push(artist_object[i].name)
+    }
+    let image_object = track.album.images
+    let image_array = []
+    for(let i=0; i<image_object.length; i++){
+        image_array.push(image_object[i].url)
+    }
+    let track_info = {
+        track_status: track_status,
+        track_url: track_url,
+        track_name: track_name,
+        album_name: album_name, 
+        artists: artist_array,
+        image: image_array[0]
+    }
+
+    return track_info
+}
+
+exports.getTrack = getTrack;
